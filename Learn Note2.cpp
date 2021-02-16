f7c0c1b5634da30a78c4486779fbe5b7771778a8
乱舞 <1638752093@qq.com>
2021-02-17 00:01:05 +0800

    更新了一些笔记

--- Learn Note2.cpp
+++ Learn Note2.cpp
@@ -2,6 +2,7 @@
 #include <string>
 #include <vector>
 #include <algorithm>
+#include <functional>
 using namespace std;
 
 class Student {//定义一个学生类 提供两个基本的构造函数
@@ -18,22 +19,26 @@ public:
 
 	static bool st(const Student& stu1, const Student& stu2) {
 		return stu1.height < stu2.height;
+		//sort()默认以<来比较大小 也就是从小到大
+		//那我们这里改成 return stu1.height > stu2.height;
+		//即可改变排序顺序 也就是从大到小
 	};
 
 	void print() {
-		cout << "==========================================================" << endl;
 		cout << "Name:" << name << "  "
 			<< "Id:" << id << "  "
 			<< "Age:" << age << "  "
 			<< "Height:" << height << endl;
-		cout << "==========================================================" << endl;
 	}
 };
 
 int main() 
 {
-	//C++中 当我们需要存储数据的时候 优先考虑用vector
-	//vector可以理解成变长数组 也就是它的大小是可以动态变化的
+	//C++ STL常用的三件套就是容器 容器算法 容器迭代器
+	//我们将一一举例说明
+
+	//C++中 当我们需要存储数据的时候 优先考虑用STL容器
+	//STL容器中的vector可以理解成变长数组 也就是它的大小是可以动态变化的
 	//有点类似于new的动态数组 但是vector作为STL的一大容器 提供了很多内置算法
 	//也就是说 vector是一个极其好用的数组
 	vector<Student> vec_stu;
@@ -51,7 +56,7 @@ int main()
 		vec_stu[j].height = 180 - j;//设置身高
 		vec_stu[j].print();
 	}
-	
+	cout << "==========================================================" << endl;
 	//STL提供的容器非常好用 如vector map set等
 	//STL提供的容器算法也是很常见的 可以省下很多造轮子的功夫
 	//如sort()是一种容器算法 用来对容器中的数据进行排序
@@ -61,10 +66,11 @@ int main()
 	//这里我们传入了一个函数名st 也就是我们将根据st来排序
 	//因为Student类并没有定义<运算 所以sort()并不直接支持排序
 	//查看Student::st() 可以很明显的看出 我们是根据Student::height来排序的
-	vector<Student>::iterator it = vec_stu.begin();//从头开始
-	for (; it != vec_stu.end(); it++) {//遍历到尾
+	vector<Student>::iterator it;//定义一个vector的迭代器
+	for (it = vec_stu.begin(); it != vec_stu.end(); it++) {//从头开始遍历到尾
 		it->print();//这里我们可以看到 身高越低的排在越前面
 	}
+	cout << "==========================================================" << endl;
 	//访问vector不仅仅可以使用下标 也可以使用迭代器访问
 	//迭代器也是STL的一部分 常用来访问容器中存储的数据
 	//容器算法基本都需要靠迭代器来完成
@@ -73,5 +79,17 @@ int main()
 	//begin()可以获取头迭代器 指向容器中的第一个数据
 	//end()可以获取尾后迭代器 它指向容器最后一个数据之后的位置 
 	//显然 尾后迭代器不指向任何数据
+
+	//下面说一下如何用Lambda表达式来填充sort()的第三个操作 定义我们自己的排序
+	//我们说sort()的第三个参数支持传入一个可调用对象 
+	//Lambda表达式可以理解为一种小函数 那么自然可以作为sort()的第三个参数
+	auto f1 = [](const Student& stu1, const Student& stu2) -> bool {return stu1.height > stu2.height; };
+	//可以用function<bool(const Student&, const Student&)>代替auto
+	sort(vec_stu.begin(), vec_stu.end(), f1);
+	for (it = vec_stu.begin(); it != vec_stu.end(); it++) {//从头开始遍历到尾
+		it->print();//身高从高到低排 因为我们改变了不等号方向 从<改成了>
+	}
+	cout << "==========================================================" << endl;
+
 	return 0;
 }
