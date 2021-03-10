3f05cde102dd2437edfea34821f72a9449557a74
乱舞 <1638752093@qq.com>
2021-03-10 16:00:32 +0800

    更新了

---
+++ Learn Note16.cpp
@@ -0,0 +1,67 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class People {
+public:
+	People() = default;
+	People(string name_in, int age_in) : name(name_in), age(age_in) {};
+	void print(ostream& os) {
+		os << name << " " << age << endl;
+	}
+	int get_age_more(int t) {
+		return age + t;
+	}
+
+private:
+	string name = "";
+	int age = 0;
+};
+
+class A {
+public:
+	int a1 = 666;
+};
+
+class B : public A {
+public:
+	int b1 = 233;
+};
+
+int main() {
+
+	People demo1("Jiang Yilong", 19);
+	People* ptr_1 = &demo1;
+	People demo2("Bai", 20);
+	People* ptr_2 = &demo2;
+
+	auto f1 = &People::print;//绑定具体成员函数
+	(demo1.*f1)(cout);//以对象调用
+	(ptr_1->*f1)(cout);//以指针调用
+
+	auto f2 = &People::get_age_more;
+	cout << (demo1.*f2)(10) << endl;
+	cout << (ptr_1->*f2)(10) << endl;
+
+	using f1_other = void (People::*) (ostream&);//类型别名 抽象出函数结构
+	f1_other f3 = &People::print;//绑定具体成员函数
+	(demo2.*f3)(cout);
+	(ptr_2->*f3)(cout);
+
+	//typeid()获取一个对象的类型 存储在一个type_info的类中 
+	//type_info这个类无法被构造 创建它的唯一途径是通过typeid()函数
+	//type_info类的精确定义取决于编译器 但至少会提供name()方法 表示类型名的可打印形式
+	cout << typeid(f1).name() << endl;
+	cout << typeid(f2).name() << endl;
+	cout << typeid(f3).name() << endl;
+
+	//在同一条继承链中的类 可以通过dynamic_cast<type*>将某个类型的指针转换成指定的type类型的指针
+	B* ptr_b1 = new B();
+	A* ptr_a1 = dynamic_cast<A*>(ptr_b1);//在运行时将指向B的指针转换成指向A的指针
+	cout << ptr_a1->a1 << endl;
+	//此时是无法通过A类型指针访问B的特有成员的 只能访问A、B共有成员
+	//cout << ptr_a1->b1 << endl;
+	delete ptr_b1;
+
+	return 0;
+}
---
+++ Learn Note17.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+using namespace std;
+
+//定义不限定作用域的枚举类型 且整形类型为无符号整数
+enum fruit : unsigned {
+	APPLE = 0u,
+	BANANA = 1u,
+	ORANGE = 2u
+};
+
+//定义限定作用域的枚举类型
+enum class animal : unsigned {//整形类型为无符号整数
+	PIG = 0u,//u是后缀 表无符号整数
+	DOG = 1u,
+	MONKEY = 2u
+};
+
+int main() {
+	/*
+		枚举是一系列整形常量的集合 
+		定义一种枚举就是定义一种新类型
+		我们通常会将一组有特殊含义的整形数据构建成一个枚举类型
+	*/
+
+	fruit demo1 = APPLE;
+	//未限定作用域 可直接访问枚举中的量APPLE 不需要使用域运算符
+	//fruit是一个枚举类型 由此定义了一个类型为fruit的demo1变量 值为APPLE
+	int i = demo1;//未限定作用域的枚举类型可以隐式转换成int类型
+	cout << "demo1:" << demo1 << endl;
+	cout << "i:" << i << endl;
+
+	//限定了作用域 需要使用域运算符来指定使用的枚举量
+	animal demo2 = animal::DOG;
+	animal demo3 = animal::MONKEY;
+	//int j = demo2;
+	//限定作用域的枚举不会隐式转换成int
+	
+	return 0;
+}
