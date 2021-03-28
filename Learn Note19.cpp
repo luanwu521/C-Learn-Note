eea09eceefcda0745f617f2a941ea14ca681a879
乱舞 <1638752093@qq.com>
2021-03-28 22:48:12 +0800

    更新了

--- Learn Note19.cpp
+++ Learn Note19.cpp
@@ -21,6 +21,7 @@ public:
 	bool operator==(const List_Node<T>& node) const {
 		return this->data == node.data && this->back == node.back && this->next == node.next;
 	}
+
 };
 
 //迭代器类型
@@ -35,11 +36,12 @@ class List_ovo {
 public:
 	class List_iterator;
 	typedef List_iterator iterator;
-
+	
 	List_ovo<T>() = default;
+	List_ovo<T>(initializer_list<T> list_) { for (auto n : list_) { add_node(n); } }//支持列表初始化
 	List_ovo<T>(const List_ovo<T>& t) {copy(t);};//拷贝构造函数
-	List_ovo<T>& operator=(const List_ovo<T>& t) {copy(t); return *this;}//赋值运算符
 	~List_ovo<T>() = default;
+	List_ovo<T>& operator=(const List_ovo<T>& t) {copy(t); return *this;}//重载赋值运算符
 
 	void add_node(const T&);//在链表最后端连接一个新节点
 	bool remove_node(const T&);//删除指定值的节点
@@ -47,19 +49,14 @@ public:
 	int search_node(const T&);//搜寻指定值的节点的所在位置 返回下标
 	int get_size() const { return size; }//获取链表中当前节点个数
 
-	iterator get_begin_iter() {//获取头迭代器
-		return List_ovo<T>::iterator(begin);
-	}
-	iterator get_end_iter() {//获取尾后迭代器
-		return List_ovo<T>::iterator(end);
-	}
-	iterator get_res_begin_iter() {//获取逆序头迭代器
-		return List_ovo<T>::iterator(end->back, iterator_type::rd);
-	}
+	iterator get_begin_iter() {return List_ovo<T>::iterator(begin_node);}
+	iterator get_end_iter() {return List_ovo<T>::iterator(end_node);}
+	iterator get_res_begin_iter() {return List_ovo<T>::iterator(end_node->back, iterator_type::rd);}
 
+	//下标访问
 	T& operator[] (int n) {
 
-		List_Node<T>* p = begin ? begin : 0;
+		List_Node<T>* p = begin_node ? begin_node : 0;
 		if (n < size && n >= 0) {
 			int cur = 0;
 			while (p && p->next) {
@@ -72,14 +69,18 @@ public:
 		
 	}
 
+	//支持范围for
+	iterator begin() const { return List_ovo<T>::iterator(begin_node); }
+	iterator end() const { return List_ovo<T>::iterator(end_node); }
+
 private:
 	int size = 0;//链表中节点个数
-	List_Node<T>* begin = NULL;//头节点
-	List_Node<T>* end = new List_Node<T>(-1);//尾后节点
+	List_Node<T>* begin_node = NULL;//头节点
+	List_Node<T>* end_node = new List_Node<T>(-1);//尾后节点
 	
 	void copy(const List_ovo<T>& t) {
 		if (t.size != 0) {
-			List_Node<T>* cur = t.begin;
+			List_Node<T>* cur = t.begin_node;
 			while (size != t.size) {
 				add_node(cur->data);
 				cur = cur->next;
@@ -107,6 +108,8 @@ public:
 		return p;
 	}
 	
+	//迭代器的比较 实际上是比较List_Node 请查看List_Node的比较函数
+	bool operator!= (List_iterator it) { return this->cur != it.cur; }
 	bool equal(const List_ovo<T>::List_iterator& it) const {return this->cur == it.cur;}
 
 private:
@@ -120,23 +123,23 @@ template <typename T>
 void List_ovo<T>::add_node(const T& value_in) {
 	List_Node<T>* add_node = new List_Node<T>(value_in);
 	if (size == 0) {
-		begin = add_node;
-		end->next = add_node;
-		add_node->next = end;
-		end->back = add_node;
-		add_node->back = end;
+		begin_node = add_node;
+		end_node->next = add_node;
+		add_node->next = end_node;
+		end_node->back = add_node;
+		add_node->back = end_node;
 	}
 	else {
-		List_Node<T>* end_back = end->back;
+		List_Node<T>* end_back = end_node->back;
 		end_back->next = add_node;
 		add_node->back = end_back;
 
-		add_node->next = end;
-		end->back = add_node;
+		add_node->next = end_node;
+		end_node->back = add_node;
 	}
 
-	end->next = begin;
-	begin->back = end;
+	end_node->next = begin_node;
+	begin_node->back = end_node;
 	size++;
 }
 
@@ -152,7 +155,7 @@ int List_ovo<T>::search_node(const T& value_in) {
 template <typename T>
 bool List_ovo<T>::remove_node(const T& value_in) {
 	
-	List_Node<T>* cur = begin;
+	List_Node<T>* cur = begin_node;
 	bool ret_val = false;
 
 	List_Node<T>* del_back = NULL;
@@ -167,7 +170,7 @@ bool List_ovo<T>::remove_node(const T& value_in) {
 
 			del_back->next = del_next;
 			del_next->back = del_back;
-			if (del == begin) begin = del_next;
+			if (del == begin_node) begin_node = del_next;
 
 			delete del;
 			size--;
@@ -183,20 +186,20 @@ template <typename T>
 void List_ovo<T>::insert_node(const T& value_in, int pos) {
 	
 	//根据所给位置判断从头向后遍历还是从尾向前遍历
-	List_Node<T>* cur = (pos <= (size / 2 - 1) && pos < size && pos >= 0 ? begin : end->back);
+	List_Node<T>* cur = (pos <= (size / 2 - 1) && pos < size && pos >= 0 ? begin_node : end_node->back);
 	List_Node<T>* add_node = new List_Node<T>(value_in);
 	if (pos == 0) {
 		
-		begin->back = add_node;
-		end->next = add_node;
-		add_node->next = begin;
-		add_node->back = end;
-		begin = add_node;
+		begin_node->back = add_node;
+		end_node->next = add_node;
+		add_node->next = begin_node;
+		add_node->back = end_node;
+		begin_node = add_node;
 		size++;
 	}
 	else {
 
-		if (cur == begin) {
+		if (cur == begin_node) {
 			for (int i = 0; i != pos - 1; i++) cur = cur->next;
 		}
 		else {
@@ -213,18 +216,19 @@ void List_ovo<T>::insert_node(const T& value_in, int pos) {
 	
 }
 
+
 #endif
 
 int main() {
 	
 	cout << "sizeof:" << sizeof(List_ovo<int>) << endl;
-	List_ovo<int> ls;
+	List_ovo<int> ls = {111, 222, 333};//使用列表初始化
 	
 	for (int i = 0; i < 10; i++) {
 		ls.add_node(i * 100);
 	}
 
-	List_ovo<int> ls2(ls);
+	List_ovo<int> ls2(ls);//调用拷贝构造函数
 
 	cout << "ls size:" << ls.get_size() << endl;
 	for (int i = 0; i < ls.get_size(); i++) {
@@ -265,18 +269,25 @@ int main() {
 	}
 	cout << endl;
 
-	List_ovo<int> ls3 = ls2;
+	List_ovo<int> ls3 = ls2;//使用重载的赋值运算符
 	cout << "迭代器遍历ls3:" << endl;
 	auto it4 = ls3.get_begin_iter();
-	for (; !it4.equal(ls3.get_end_iter()); it4++) {
+	for (; it4 != ls3.get_end_iter(); it4++) {
 		cout << *it4 << " ";
 	}
 	cout << endl;
 
-	List_ovo<int> ls4;
+	cout << "范围for遍历ls3:" << endl;
+	for (auto v : ls3) {
+		cout << v << " ";
+	}
+	cout << endl;
+
+	List_ovo<long long> ls4;
 	for (long long g = 0; g < 1000000; g++) {
 		ls4.add_node(g);
 	}
 	cout << "ls4 size:" << ls4.get_size() << endl;
+
 	return 0;
 }
