d6a7afd03c732886ad0073f4fc1d7c1e44922344
乱舞 <1638752093@qq.com>
2021-04-12 19:24:43 +0800

    更新了

--- Learn Note27.cpp
+++ Learn Note27.cpp
@@ -16,15 +16,31 @@ void copy_s(const T* _begin, const T* _end, T* _target) {
 template<class T>
 struct arrayList {
 public:
-	arrayList() = default;
+	arrayList() : pack(new T[0]) {};
 	arrayList(unsigned len) : pack(new T[len]), packSize(len) {}
-	~arrayList() { delete[] pack; }
-
-	unsigned size() { return listSize; }
-	
-	T& operator[](unsigned pos) {
-		return check_pos(pos) ? pack[pos] : *pack;
+	arrayList(const arrayList<T>& rhs) {
+		copy(rhs);
 	}
+	~arrayList() { delete[] pack; }
+	bool operator!=(const arrayList<T>& rhs) {
+		return rhs.pack != this->pack;
+	}
+	arrayList<T>& operator=(const arrayList<T>& rhs) {
+		if (*this != rhs) {//防止自赋值
+			this->~arrayList();
+			copy(rhs);
+		}
+		return *this;
+	}
+	
+	unsigned size() { return listSize; }
+	bool check_pos(const unsigned& pos) const {
+		if (listSize == 0) return pos == 0 ? true : false;
+		else return pos >= 0 && pos < listSize;
+	}
+
+	//增删改查
+	T& operator[](unsigned pos) {return check_pos(pos) ? pack[pos] : *pack;}
 	void insert(const T value, unsigned pos) {
 		if (check_pos(pos)) {
 			if (listSize == packSize) reallocate(packSize * 2);//扩充到原来的两倍容量
@@ -32,10 +48,22 @@ public:
 				pack[0] = T(value);
 			}
 			else {
-				copy(pack + pos, pack + listSize, pack + pos + 1);
+				copy_s(pack + pos, pack + listSize, pack + pos + 1);
 				pack[pos] = T(value);
 			}
 			listSize++;
+		}
+	}
+	void remove(unsigned pos) {
+		if (check_pos(pos)) {
+			(pack + pos)->~T();
+			copy_s(pack + pos + 1, pack + listSize, pack + pos);
+			listSize--;
+		}
+	}
+	int search(const T value) {
+		for (unsigned j = 0; j < listSize; j++) {
+			if (*(pack + j) == value) return j;
 		}
 	}
 
@@ -44,9 +72,11 @@ private:
 	unsigned packSize = 0;
 	unsigned listSize = 0;
 
-	bool check_pos(const unsigned& pos) const { 
-		if (listSize == 0) return pos == 0 ? true : false;
-		else return pos >= 0 && pos < listSize;
+	void copy(const arrayList& rhs) {
+		packSize = rhs.packSize;
+		pack = new T[rhs.packSize];
+		copy_s(rhs.pack, rhs.pack + rhs.listSize, pack);
+		listSize = rhs.listSize;
 	}
 	void reallocate(unsigned new_size) {
 		T* temp = new T[new_size > 0 ? new_size : 1];
@@ -55,18 +85,33 @@ private:
 		pack = temp;
 		packSize = new_size;
 	}
+
 };
 
 int main()
 {
 	arrayList<int> arr1(10);
-	for (int i = 0; i < 100; i++) {
+	for (int i = 0; i < 20; i++) {
 		arr1.insert(i, 0);
 	}
-	
-	for (int i = 0; i < 100; i++) {
-		cout << arr1[i] << endl;
+	arrayList<int> arr2(arr1);
+	arrayList<int> arr3;
+	arr1.remove(5);
+	for (unsigned i = 0; i < arr1.size(); i++) {
+		cout << arr1[i] << " ";
 	}
+	cout << endl;
+
+	for (unsigned i = 0; i < arr2.size(); i++) {
+		cout << arr2[i] << " ";
+	}
+	cout << endl;
+
+	arr3 = arr2;
+	for (unsigned i = 0; i < arr3.size(); i++) {
+		cout << arr3[i] << " ";
+	}
+	cout << endl;
 
 	return 0;
 }
